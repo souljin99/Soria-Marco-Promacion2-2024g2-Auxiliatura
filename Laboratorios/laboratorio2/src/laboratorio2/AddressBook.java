bf20d3fef6b33c7a1272a8f7968b276e019c20db
narco <narco.army.once@gmail.com>
2024-09-08 17:15:53 -0400

    lab2

---
+++ Laboratorios/laboratorio2/src/laboratorio2/AddressBook.java
@@ -0,0 +1,78 @@
+package laboratorio2;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+public class AddressBook {
+    private HashMap<String, Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new HashMap<>();
+    }
+
+    public void addContact(Contact contact) {
+        if (contacts.containsKey(contact.getEmail())) {
+            System.out.println("A contact with this email already exists.");
+        } else {
+            contacts.put(contact.getEmail(), contact);
+            System.out.println("Contact added successfully.");
+        }
+    }
+
+    public void viewContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("The address book is empty.");
+            return;
+        }
+        for (Contact contact : this.contacts.values()) {
+            contact.print();
+        }
+    }
+
+    public void searchContact(String email) {
+        if (contacts.containsKey(email)) {
+        	System.out.println(contacts.get(email));
+        } else {
+            System.out.println("Contact not found.");
+        }
+    }
+
+    public void deleteContact(String email) {
+        if (contacts.containsKey(email)) {
+            contacts.remove(email);
+            System.out.println("Contact deleted.");
+        } else {
+            System.out.println("No contact found with the provided email.");
+        }
+    }
+    
+    public void storeContact() {
+    	try {
+            FileOutputStream file = new FileOutputStream("contactos.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(file);
+            oos.writeObject(contacts);
+            oos.close();
+            file.close();
+            System.out.println("The list of contacts has been saved in the'contacts.ser'folder");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void loadContact() {
+    	try {
+            FileInputStream file = new FileInputStream("contacts.ser");
+            ObjectInputStream ois = new ObjectInputStream(file);
+            HashMap<String, Contact> savedContats = (HashMap<String, Contact>) ois.readObject();
+            ois.close();
+            file.close();
+            System.out.println("Contact list previously saved: " + savedContats);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Laboratorios/laboratorio2/src/laboratorio2/Contact.java
@@ -0,0 +1,56 @@
+package laboratorio2;
+
+
+public class Contact {
+    private String name;
+    private String email;
+    private Integer age;
+    private String phone;
+
+    public Contact(String name, String email, int age, String phone) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.phone = phone;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void print() {
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Age:" + age);
+        System.out.println("Phone: " + phone);
+        System.out.println("--------------");
+    }
+}
